d1a06cd167da05a47e3d4763d7ad35f208d96575
Daykane <alexis.laboureur@gmail.com>
2015-02-28 15:32:54 +0100

    Commentaire changé

--- src/UI/UserUI.java
+++ src/UI/UserUI.java
@@ -20,7 +20,7 @@ import java.awt.event.MouseEvent;
 public class UserUI extends JFrame {
 
 	/**
-	 * Init Attribut
+	 * Init Attribute
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
