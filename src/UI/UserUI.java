1833418b8472837078ac5dbfb08841e74b419c0a
Ludo <ludowigh@hotmail.fr>
2015-02-28 15:49:32 +0100

    new message

--- src/UI/UserUI.java
+++ src/UI/UserUI.java
@@ -20,7 +20,7 @@ import java.awt.event.MouseEvent;
 public class UserUI extends JFrame {
 
 	/**
-	 * Init Attribute tott
+	 * Init Attribute t
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
