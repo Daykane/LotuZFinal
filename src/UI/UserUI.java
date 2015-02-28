e154b85bc39f818f61744b0b8e81b05c28673afd
Alexandre <Alexandre@Alexandre-HP>
2015-02-28 16:14:34 +0100

    dssfx

--- src/UI/UserUI.java
+++ src/UI/UserUI.java
@@ -20,7 +20,7 @@ import java.awt.event.MouseEvent;
 public class UserUI extends JFrame {
 
 	/**
-	 * Init Attribute derde
+	 * Init Attribute derder
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
