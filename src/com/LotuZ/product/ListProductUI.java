3abbdb52e56190e33b4240d88a18a1497280bcd2
Alexandre <Alexandre@162.38.24.43>
2015-03-26 18:46:09 +0100

    listProductUI alex

---
+++ src/com/LotuZ/product/ListProductUI.java
@@ -0,0 +1,94 @@
+package com.LotuZ.product;
+
+import interfaceDeBase.Bandeau;
+import interfaceDeBase.PageAccueiltest;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.LotuZ.FacadeBL;
+import com.LotuZ.JdbcKit;
+import com.LotuZ.login.UserNotFoundException;
+import com.LotuZ.product.category.bl.ListCategoryProduct;
+import com.LotuZ.user.FacadeUser;
+import com.LotuZ.user.UserLog;
+import com.LotuZ.user.activityLeader.bl.ListActivityLeader;
+import com.LotuZ.user.user.bl.User;
+
+import javax.swing.JList;
+import javax.swing.JButton;
+
+public class ListProductUI extends JFrame {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+
+	public static void main(String[] args) {
+		// Info Connection
+		String url = "jdbc:mysql://lotuz.c48krzyl3nim.eu-west-1.rds.amazonaws.com:3306/LotuZ";
+		String login = "ROLL";
+		String passwd = "rolldevelopment";
+
+		// Choose the kit
+		JdbcKit jdbcKit = new JdbcKit(url,login,passwd);
+		jdbcKit.openConnection(url, login, passwd);
+
+		// Init the FacadeBL with the kit
+		FacadeBL.init(jdbcKit);
+		//FacadeUser.init();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListProductUI frame = new ListProductUI();
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	/**
+	 * Create the frame.
+	 * @throws UserNotFoundException 
+	 * @throws SQLException 
+	 */
+	public ListProductUI() throws SQLException, UserNotFoundException {
+		FacadeUser.login("jack","jack");
+		User user = UserLog.getUserLog();
+		Bandeau bandeau = new Bandeau();
+		System.out.println("Mail page acceuil test : " + user.getMail());
+
+		setTitle("Zen Lounge");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(300,800, 750, 600);
+		contentPane = new JPanel();
+		contentPane.setToolTipText("");
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(bandeau.createBandeau(user, "Liste Produit"), BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+	}	
+}
