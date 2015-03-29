1e80371a1ad9e1956d3d31a2f8726ad998e1d97d
lowx <lowx@SolidGround-Satellite-L300>
2015-03-29 18:48:15 +0200

    Alex are you ready

--- src/com/LotuZ/product/category/ui/ViewCategoryUI.java
+++ src/com/LotuZ/product/category/ui/ViewCategoryUI.java
@@ -29,6 +29,7 @@ import javax.swing.border.TitledBorder;
 import com.LotuZ.FacadeBL;
 import com.LotuZ.JdbcKit;
 import com.LotuZ.login.UserNotFoundException;
+import com.LotuZ.product.ListProductUI;
 import com.LotuZ.product.category.bl.CategoryProduct;
 import com.LotuZ.product.category.bl.ListCategoryProduct;
 import com.LotuZ.user.UserLog;
@@ -338,12 +339,19 @@ public class ViewCategoryUI extends JFrame {
 		validationPan.setLayout(new GridBagLayout());
 		contentPaneCenter.add(validationPan, BorderLayout.SOUTH);
 			
-			//Cancel
+		//Cancel
 		JButton btnCancel =new JButton("Cancel");
 		GridBagConstraints gbc_btnCancel= new GridBagConstraints();
 		gbc_btnCancel.gridx = 0;
 		gbc_btnCancel.gridy = 5;
 		categoryPan.add(btnCancel, gbc_btnCancel);
+		
+		//Product
+		JButton btnProduct =new JButton("Products");
+		GridBagConstraints gbc_btnProduct= new GridBagConstraints();
+		gbc_btnProduct.gridx = 2;
+		gbc_btnProduct.gridy = 5;
+		categoryPan.add(btnProduct, gbc_btnProduct);
 		
 //			//Submit
 //		JButton btnOk=new JButton("Ok");
@@ -372,6 +380,32 @@ public class ViewCategoryUI extends JFrame {
 				}
 				CategoryUI.setVisible(true);
 				CategoryUI.setLocationRelativeTo(null);
+				dispose();
+				
+				
+			}
+		};
+		btnCancel.addActionListener(btnCancelListeners);
+		
+		//btnProductsListeners
+		
+		ActionListener btnProductListeners = new ActionListener() 
+		{
+			
+			public void actionPerformed(ActionEvent e)
+			{
+				ListProductUI listProductUI = null;
+				try {
+					listProductUI = new listProductUI(categorySel.getIdCategoryProduct());
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UserNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				listProductUI.setVisible(true);
+				listProductUI.setLocationRelativeTo(null);
 				dispose();
 				
 				
