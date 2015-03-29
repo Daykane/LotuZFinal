8857abb50979d5e0a1e07fca59ebbbae82697762
Daykane <alexis.laboureur@gmail.com>
2015-03-29 17:48:23 +0200

    javadoc

--- src/com/LotuZ/EventInscription/FacadeEventInscription.java
+++ src/com/LotuZ/EventInscription/FacadeEventInscription.java
@@ -13,19 +13,39 @@ public class FacadeEventInscription {
 	
 	private static EventInscriptionManager eventInscriptionManager;
 	
+	/**
+	 * Init Manager with persistance kit
+	 * @param kit : the persistance
+	 */
 	public static void init(PersistKit kit){
 		eventInscriptionManager = new EventInscriptionManager(kit);
 	}
 
+	/**
+	 * register the user in the event
+	 * @param user
+	 * @param event
+	 * @throws SQLException
+	 */
 	public static void addUserInEvent(User user, Event event) throws SQLException {
 		eventInscriptionManager.addUserInEvent(user,event);
 		
 	}
 
+	/**
+	 * ListEvent where the user is register
+	 * @param user
+	 * @throws SQLException
+	 */
 	public static List<Event> getUserEvent(User user) throws SQLException {
 		return eventInscriptionManager.getUserEvent(user);		
 	}
-
+	/**
+	 * List identifer User register in the Event
+	 * @param event
+	 * @return List user identifier
+	 * @throws SQLException
+	 */
 	public static List<String> getEventUser(Event event) throws SQLException {
 		return eventInscriptionManager.getEventUser(event);	
 	}
